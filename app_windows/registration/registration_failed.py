6c1fb8b6c874678157e0ae64574dd866d66bcf05
bloodofspring <t.shagin@gmail.com>
2024-10-31 20:47:06 +0300

    feat: Добавлен диалог при неверных вводных данных при регистрации

---
+++ app_windows/registration/registration_failed.py
@@ -0,0 +1,18 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import QDialog
+
+
+class RegistrationFailed(QDialog):
+    def __init__(self, error_message: str):
+        super().__init__()
+        uic.loadUi('registration_failed_template.ui', self)
+        self.setFixedSize(self.size())  # Запретить изменение окна
+
+        self.error_label.setText(error_message)
+
+        self.buttonBox.accepted.connect(self.__close)
+        self.buttonBox.rejected.connect(self.__close)
+
+
+    def __close(self):
+        self.close()
--- app_windows/registration/registration_main.py
+++ app_windows/registration/registration_main.py
@@ -1,8 +1,10 @@
 import sys
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QDialog
 
+from app_windows.registration.registration_failed import RegistrationFailed
+from database import create_tables
 from database.models import AppUser, AppUserConfig
 
 
@@ -20,34 +22,48 @@ class RegistrationMain(QMainWindow):
 
     @property
     def users_with_same_login(self):
-        login = self.login_field
-        return AppUserConfig.select().where(AppUserConfig.login == login)[0].user
+        login = self.login_field.text().strip()
+        users_with_this_login = AppUserConfig.select().where(AppUserConfig.login == login)
+
+        if len(users_with_this_login) == 0:
+            return None
+
+        return users_with_this_login[0].user
 
     def check_user_data(self):
         if not self.users_with_same_login:
-            return 1  # Нет пользователя с таким логином
+            self.show_reg_failed_window(error_message="Неверный логин!")
+            return
 
-        if self.users_with_same_login[0].config[0].password != self.password_filed:
-            return 2  # Неверный пароль
+        if self.users_with_same_login.config[0].password != self.password_filed.text().strip():
+            self.show_reg_failed_window(error_message="Неверный пароль!")
+            return
 
         return 3  # Все проверки пройдены
 
     def registrate_new_user(self):
         if self.users_with_same_login:
+            self.show_reg_failed_window(error_message=f"Логин {self.login_field.text().strip()} уже занят!")
             return  # логин уже занят
 
         AppUserConfig.create(
-            login=self.login_field,
-            password=self.password_filed,
+            login=self.login_field.text().strip(),
+            password=self.password_filed.text().strip(),
             yandex_api_key="<no_key>",
             user=AppUser.create()
         )
 
         return 3  # Аккаунт создан, все проверки пройдены
 
+    @staticmethod
+    def show_reg_failed_window(error_message: str):
+        ex_ = RegistrationFailed(error_message=error_message)
+        ex_.exec()
+
 
 # Для тестов
 if __name__ == '__main__':
+    create_tables()
     app = QApplication(sys.argv)
     ex = RegistrationMain()
     ex.show()
