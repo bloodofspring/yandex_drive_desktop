763280de2e10558519dabd71eb25897fdf59b7c0
bloodofspring <t.shagin@gmail.com>
2024-10-31 20:12:21 +0300

    feat: Добавлена проверка ввода данных. Класс протестирован

--- app_windows/registration/page.py
+++ app_windows/registration/page.py
@@ -3,6 +3,8 @@ import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow, QApplication
 
+from database.models import AppUser, AppUserConfig
+
 
 class RegistrationMain(QMainWindow):
     def __init__(self):
@@ -16,11 +18,32 @@ class RegistrationMain(QMainWindow):
         self.enter_button.clicked.connect(self.check_user_data)
         self.reg_button.clicked.connect(self.registrate_new_user)
 
+    @property
+    def users_with_same_login(self):
+        login = self.login_field
+        return AppUserConfig.select().where(AppUserConfig.login == login)[0].user
+
     def check_user_data(self):
-        pass
+        if not self.users_with_same_login:
+            return 1  # Нет пользователя с таким логином
+
+        if self.users_with_same_login[0].config[0].password != self.password_filed:
+            return 2  # Неверный пароль
+
+        return 3  # Все проверки пройдены
 
     def registrate_new_user(self):
-        pass
+        if self.users_with_same_login:
+            return  # логин уже занят
+
+        AppUserConfig.create(
+            login=self.login_field,
+            password=self.password_filed,
+            yandex_api_key="<no_key>",
+            user=AppUser.create()
+        )
+
+        return 3  # Аккаунт создан, все проверки пройдены
 
 
 # Для тестов
