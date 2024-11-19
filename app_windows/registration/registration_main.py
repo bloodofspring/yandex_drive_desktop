39ffb08f0a8e053df526650688116625817ecd40
bloodofspring <t.shagin@gmail.com>
2024-11-19 15:49:32 +0300

    feat: Интегрирована новая бд

--- app_windows/registration/registration_main.py
+++ app_windows/registration/registration_main.py
@@ -3,7 +3,7 @@ from PyQt6.QtWidgets import QDialog
 
 from app_windows.registration.registration_failed import WrongRegDataDialog, LoginIsTakenDialog
 from config import TEMPLATES_PATH
-from database.models import AppUser, AppUserConfig
+from database.models import AppUser, AppUserConfig, Session
 
 
 class RegistrationDialog(QDialog):
@@ -19,23 +19,25 @@ class RegistrationDialog(QDialog):
         self.reg_button.clicked.connect(self.registrate_new_user)
 
     @property
-    def users_with_same_login(self):
+    def users_with_same_login(self) -> AppUserConfig | None:
         login = self.login_field.text().strip()
-        users_with_this_login = AppUserConfig.select().where(AppUserConfig.login == login)
+        users_with_this_login = AppUserConfig.select().where(AppUserConfig.login == login)[:]
 
-        if len(users_with_this_login) == 0:
+        if not users_with_this_login:
             return None
 
-        return users_with_this_login[0].user
+        return users_with_this_login[0]
 
     def check_user_data(self):
-        if not self.users_with_same_login:
+        if self.users_with_same_login is None:
             WrongRegDataDialog().exec()
             return
 
-        if self.users_with_same_login.config[0].password != self.password_filed.text().strip():
+        if self.users_with_same_login.password != self.password_filed.text().strip():
             WrongRegDataDialog().exec()
             return
+
+        Session.create(user=AppUser.get(login=self.login_field.text().strip()))
 
         self.close()
 
@@ -44,11 +46,11 @@ class RegistrationDialog(QDialog):
             LoginIsTakenDialog().exec()
             return  # логин уже занят
 
-        AppUserConfig.create(
-            login=self.login_field.text().strip(),
+        config = AppUserConfig.create(
             password=self.password_filed.text().strip(),
             yandex_api_key="<no_key>",
-            user=AppUser.create()
         )
+        app_user = AppUser.create(login=self.login_field.text().strip(), config=config)
+        Session.create(user=app_user)
 
         self.close()
--- database/models/users.py
+++ database/models/users.py
@@ -1,17 +1,13 @@
 from peewee import CharField, ForeignKeyField
 
 from database.models.base import BaseModel
-from database.models.storage import FileDirectory
-
-
-class AppUser(BaseModel):
-    pass
 
 
 class AppUserConfig(BaseModel):
-    login = CharField()
     password = CharField()
     yandex_api_key = CharField()
-    disk_data_root_folder = ForeignKeyField(FileDirectory, backref="user")
 
-    user = ForeignKeyField(AppUser, backref="config")
+
+class AppUser(BaseModel):
+    login = CharField()
+    config = ForeignKeyField(AppUserConfig, backref="user")
