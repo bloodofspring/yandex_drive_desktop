777e6c7cfcf94ad21666714e182015a494cd277c
bloodofspring <t.shagin@gmail.com>
2024-11-21 01:40:59 +0300

    feat: реализован GetAuthTokenDialog
    НЕ ПРОТЕСТИРОВАН

--- app_windows/change_token_form/change_token_dialog.py
+++ app_windows/change_token_form/change_token_dialog.py
@@ -1,7 +1,16 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QDialog
+from yadisk import YaDisk
 
+from app_windows.change_token_form.not_implemented_yet import AddLater
+from app_windows.dialog_samples import EmptyDialog
 from config import TEMPLATES_PATH
+from database.models import AppUser, AppUserConfig
+from util import get_last_session
+
+
+class WrongTokenDialog(EmptyDialog):
+    INFO = "Указан неверный токен!"
 
 
 class GetAuthTokenDialog(QDialog):
@@ -13,4 +22,27 @@ class GetAuthTokenDialog(QDialog):
         self.check_button_signals()
 
     def check_button_signals(self):
-        pass
+        self.save_btn.clicked.connect(self.check_and_save)
+        self.cancel_btn.clicked.connect(self.close)
+        self.instruction_button.clicked.connect(self.instruction)
+
+    def check_and_save(self):
+        if not YaDisk(token=self.token_input.text().strip('\n')).check_token():
+            ex_ = WrongTokenDialog()
+            ex_.exec()
+            return
+
+        last_session = get_last_session()
+        if last_session is None:
+            return
+
+        user: AppUser = last_session.user
+        config: AppUserConfig = user.config
+        config.yandex_api_key = self.token_input.text().strip('\n')
+        config.has_valid_token = True
+        AppUserConfig.save(config)
+
+        self.close()
+
+    def instruction(self):
+        AddLater().exec()
