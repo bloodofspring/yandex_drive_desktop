c065c08f62553da786e959b7f801de0e3db37cb7
bloodofspring <t.shagin@gmail.com>
2024-11-21 01:28:21 +0300

    feat: Реализован GetAuthTokenDialog

--- app_windows/change_loginpas_form/change_loginpas_dialog.py
+++ app_windows/change_loginpas_form/change_loginpas_dialog.py
@@ -1,7 +1,11 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QDialog
 
+from app_windows.change_loginpas_form.change_process_failed import LoginIsTakenDialog
+from app_windows.change_loginpas_form.data_updated import DataSuccessfullyUpdated
 from config import TEMPLATES_PATH
+from database.models import AppUser, AppUserConfig
+from util import get_last_session
 
 
 class GetAuthTokenDialog(QDialog):
@@ -13,4 +17,34 @@ class GetAuthTokenDialog(QDialog):
         self.check_button_signals()
 
     def check_button_signals(self):
-        pass
+        self.save_btn.clicked.connect(self.check_and_save_data)
+        self.cancel_button.clicked.connect(self.close)
+
+    def users_with_same_login(self) -> AppUser | None:
+        login = self.login_field.text().strip()
+        users_with_this_login = AppUser.select().where(AppUser.login == login)[:]
+
+        if not users_with_this_login:
+            return None
+
+        return users_with_this_login[0]
+
+    def check_and_save_data(self):
+        if self.users_with_same_login() is not None:
+            LoginIsTakenDialog().exec()
+            return  # логин уже занят
+
+        latest_session = get_last_session(show_alert=False)
+        if latest_session is None:
+            return
+
+        user = latest_session.user
+        config = user.config
+
+        config.password = self.password_field.text().strip()
+        user.login = self.login_field.text().strip()
+
+        AppUser.save(user)
+        AppUserConfig.save(config)
+
+        DataSuccessfullyUpdated().exec()
---
+++ app_windows/change_loginpas_form/data_updated.py
@@ -0,0 +1,5 @@
+from app_windows.dialog_samples import EmptyDialog
+
+
+class DataSuccessfullyUpdated(EmptyDialog):
+    INFO = "Данные успешно обновлены!"
